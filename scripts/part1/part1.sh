c85af606328d2e85afdb354a6c6176d1b29261d7
1wsx10 <1wsx10@gmail.com>
2018-05-21 22:00:02 +1000

    On branch timing
    	modified:   part1.sh
    	    added timing info

--- scripts/part1/part1.sh
+++ scripts/part1/part1.sh
@@ -15,23 +15,63 @@ then
 	echo "reboot DBs"
 	#TODO
 
+	exec 3>&1 4>&2
+	var=$( { time myexec -args 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+
 	echo "test query\n1\twith-reboot"
-	time ./run-queries.sh query-tables1.sql
+	exec 3>&1 4>&2
+	q1r=$( { time ./run-queries.sh query-tables1.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q1r
 	echo "test query\n2\twith-reboot"
-	time ./run-queries.sh query-tables2.sql
+	exec 3>&1 4>&2
+	q2r=$( { time ./run-queries.sh query-tables2.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q2r
 	echo "test query with secondary index\n1\twith-reboot"
-	time ./run-queries.sh query-tables-secidx1.sql
+	exec 3>&1 4>&2
+	q1rs=$( { time ./run-queries.sh query-tables-secidx1.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q1rs
 	echo "test query with secondary index\n2\twith-reboot"
-	time ./run-queries.sh query-tables-secidx2.sql
+	exec 3>&1 4>&2
+	q2rs=$( { time ./run-queries.sh query-tables-secidx2.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q2rs
 
 	echo "test query\n1\tno-reboot"
-	time ./run-queries.sh query-tables1.sql
+	exec 3>&1 4>&2
+	q1=$( { time ./run-queries.sh query-tables1.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q1
 	echo "test query\n2\tno-reboot"
-	time ./run-queries.sh query-tables2.sql
+	exec 3>&1 4>&2
+	q2=$( { time ./run-queries.sh query-tables2.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q2
 	echo "test query with secondary index\n1\tno-reboot"
-	time ./run-queries.sh query-tables-secidx1.sql
+	exec 3>&1 4>&2
+	q1s=$( { time ./run-queries.sh query-tables-secidx1.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q1s
 	echo "test query with secondary index\n2\tno-reboot"
-	time ./run-queries.sh query-tables-secidx2.sql
+	exec 3>&1 4>&2
+	q2s=$( { time ./run-queries.sh query-tables-secidx2.sql 1>&3 2>&4; } 2>&1 )  # Captures time only.
+	exec 3>&- 4>&-	
+	echo $q2s
+
+	printf "\n\n\nDirectly after reboot:"
+	printf "\n\nQuery 1, no-secondary idx$q1r"
+	printf "\n\nQuery 2, no-secondary idx$q2r"
+	printf "\n\nQuery 1, secondary idx$q1r"
+	printf "\n\nQuery 2, secondary idx$q2r"
+
+	printf "\n\n\nNot after reboot:"
+	printf "\n\nQuery 1, no-secondary idx$q1r"
+	printf "\n\nQuery 2, no-secondary idx$q2r"
+	printf "\n\nQuery 1, secondary idx$q1r"
+	printf "\n\nQuery 2, secondary idx$q2r\n\n"
 
 else
 	echo "Usage: bash part1.sh input_csv_file"
