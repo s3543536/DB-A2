eb4436ea6a1fa0ce1844b04f1a1fd2639fb1a362
EC2 Default User <ec2-user@ip-10-88-170-184.ap-southeast-2.compute.internal>
2018-05-21 09:26:37 +0000

    now 2 DBs, one with no secondary index, one with a secondary index on business name.
    then there are 4 query files. 2 to run each query on the DB with no secondary index
    	2 to run each query on the DB with a secondary index
    
    these are all separate so i can time them all individually.
    
     On branch add_secondary_index
    
    	modified:   part1.sh
    		- added running of all query files, and a spot to reboot the DBs
    
    	new file:   derby-script-secidx.sql
    		- creates tables and inserts data all the same as the original derby-script.sql
    		- also adds a secondary index to BN_NAME
    
        query files:
    	new file:   query-tables1.sql
    	renamed:    query-tables.sql -> query-tables2.sql
    
        query files for secondary index DB:
    	new file:   query-tables-secidx1.sql
    	new file:   query-tables-secidx2.sql

--- scripts/part1/part1.sh
+++ scripts/part1/part1.sh
@@ -6,10 +6,33 @@ then
 	chmod 700 run-queries.sh
 	echo "reformatting and splitting CSV data"
 	time ./split-tables.sh "$1"
-	echo "creating tables and loading data"
+
+	echo "creating tables and loading data into DB without secondary index"
 	time ./run-queries.sh derby-script.sql
-	echo "test query"
-	time ./run-queries.sh query-tables.sql
+	echo "creating tables and loading data into DB with secondary index"
+	time ./run-queries.sh derby-script-secidx.sql
+
+	echo "reboot DBs"
+	#TODO
+
+	echo "test query\n1\twith-reboot"
+	time ./run-queries.sh query-tables1.sql
+	echo "test query\n2\twith-reboot"
+	time ./run-queries.sh query-tables2.sql
+	echo "test query with secondary index\n1\twith-reboot"
+	time ./run-queries.sh query-tables-secidx1.sql
+	echo "test query with secondary index\n2\twith-reboot"
+	time ./run-queries.sh query-tables-secidx2.sql
+
+	echo "test query\n1\tno-reboot"
+	time ./run-queries.sh query-tables1.sql
+	echo "test query\n2\tno-reboot"
+	time ./run-queries.sh query-tables2.sql
+	echo "test query with secondary index\n1\tno-reboot"
+	time ./run-queries.sh query-tables-secidx1.sql
+	echo "test query with secondary index\n2\tno-reboot"
+	time ./run-queries.sh query-tables-secidx2.sql
+
 else
 	echo "Usage: bash part1.sh input_csv_file"
 fi
